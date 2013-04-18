cb3669f8879d1971eaf7bffc2188b701a8741d51
Watson W. Blair <bestudios.wb@gmail.com>
2013-04-18 19:31:50 -0400

    this is from before the roolback

--- src/com/example/foodreference2/Reference.java
+++ src/com/example/foodreference2/Reference.java
@@ -4,12 +4,16 @@ import com.example.foodreference2.R;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Reference extends Activity {
 
@@ -30,5 +34,6 @@ public class Reference extends Activity {
 		return super.onOptionsItemSelected(item);
 
 	}
+	
 
 }
---
+++ src/com/example/foodreference2/view_catagory.java
@@ -0,0 +1,11 @@
+package com.example.foodreference2;
+
+public class view_catagory {
+
+	public static Class<?> onCreate(String string) {
+		return null;
+		// TODO Auto-generated method stub
+		
+	}
+
+}
